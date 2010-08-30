f452d4651dd4b08f9eced4d9887e40ec63c44981
Adam Kubica <caffecoder@gmail.com>
2010-08-31 00:38:22 +0200

    Added example code HTML/JavaScript that uses macaddress-applet.

---
+++ macaddress-applet.js
@@ -0,0 +1,28 @@
+/**
+ * This software is released under the New BSD License.
+ *
+ * @author: Adam Kubica <caffecoder@gmail.com>
+ */
+function show_macaddresses(){
+    if ( typeof(document.macaddress_applet.isActive) == "undefined" )
+    {
+        document.write( "Unable to detect MAC addresses." );
+    }
+    else
+    {
+        var interfaces = eval( String( document.macaddress_applet.getInterfacesJSON() ) );
+        var interface_string = "";
+
+        for( var idx = 0; idx < interfaces.length; idx++ )
+        {
+            var tmp = interfaces[idx].split(";");
+            interface_string += "NIC " + (idx+1) + ": " + tmp[1] + " (" + tmp[0] + ")<br />\n ";
+        }
+
+        if ( interface_string.length > 0 )
+        {
+            document.write( "Detected MAC addresses:<br />" );
+            document.write( interface_string );
+        }
+    }
+}
